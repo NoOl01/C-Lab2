712ae7965de73e1229453c6e3de22a309ce8bc84
NoOl01 <tagaevanton0@gmail.com>
2024-12-24 19:27:03 +0200

    first commit

---
+++ Lab2/Program.cs
@@ -0,0 +1,33 @@
+﻿for (int i = 0; i < 10; i++)
+{
+    Main sem = new Main(i);
+}
+
+class Main
+{
+    static Semaphore sem = new Semaphore(3, 3);
+    Thread thread;
+
+    private int count = 1;
+    public Main(int i)
+    {
+        thread = new Thread(Generator);
+        thread.Name = $"Поток {i}";
+        thread.Start();
+    }
+
+    Random rnd = new Random();
+    public void Generator()
+    {
+        while (count > 0)
+        {
+            sem.WaitOne();
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(Thread.CurrentThread.Name + " " + rnd.Next(0, 100));
+            }
+            sem.Release();
+            count--;
+        }
+    }
+}
